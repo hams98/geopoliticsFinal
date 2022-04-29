5ddbcd56c210ffcd88cc1c867ec53250113e4ec9
hams98 <97984418+hams98@users.noreply.github.com>
2022-04-29 18:47:16 -0400

    Update atmosphereVertex.glsl.js

--- shaders/atmosphereVertex.glsl.js
+++ shaders/atmosphereVertex.glsl.js
@@ -3,15 +3,19 @@
 //fragment shader
 
 
-varying vec3 vertexNormal;
+//varying vec3 vertexNormal;
 
+
+
+//varying vec3 vertexNormal;
+const atmosphereVertex = `
+//uniform sampler2D globeTexture;
 void main(){
  
-
    // normalize(normalMatrix * normal); to make brighness similar
     vertexNormal = normalize(normalMatrix * normal);
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-}
+}`
+export default  atmosphereVertex
 
