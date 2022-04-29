a8862ce0dafc1d7d8d6899858bc7f3835188a8bb
hams98 <97984418+hams98@users.noreply.github.com>
2022-04-29 18:07:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,9 @@
 import * as THREE from 'three'
-//we have a default vertex attribute vec2 uv
-//if we have to pass this vertex shader attribute to
-//fragment shader
 
-varying vec2 vertexUV;
-varying vec3 vertexNormal;
-
-//represents every vertex on the globe
-//https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram
-void main(){
-    //x,y,z postions and for translations and transforms
-    // we need to have dynamix x,y,z 
-    // position is a vector 3
-
-    vertexUV = uv;
-vertexNormal = normalize(normalMatrix * normal);
-    //modelmatrix, view matrix,projection matrix are matrix data types
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
-}
-
-// example triangle matrix =
-// [-1, 0, 0,
-//  0, 1, 0, 
-//  1, 0, 0] 
-
-//projection matrix is mat4 and modelViewMatrix a;so mat4 (4*4 matrix)
-// vec4(position, 1.0) === vec4(1, 0, 0, 1.0)
-//position* vec3(2,2,1) === vec3(1,0,0) vec3(2,2,1))
-//x = 1*2=2
-//y =0*2=0
-//z=0*1=0
-//position = vec(2,0,0)
-
-//takeaway is to use the boiler plate code for shader to work
 
 import { Float32BufferAttribute, MeshBasicMaterial } from 'three';
 console.log("hey")
-//install a vite-plug in string so vite understangs to import the shader
+install a vite-plug in string so vite understangs to import the shader
 //import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
 import vertexShader from './shaders/vertex.glsl'
 import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
