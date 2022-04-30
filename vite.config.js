8b9ab1e25339158c5229008ef9d72aed281c4b0c
hams98 <97984418+hams98@users.noreply.github.com>
2022-04-30 00:55:31 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vitePluginString from 'vite-plugin-string'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-base: '/geopoliticsFinal/',
+
 plugins: [
   vitePluginString()
 ]
